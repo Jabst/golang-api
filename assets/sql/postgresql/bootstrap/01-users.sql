5d3053bdbbd9caf3b7508542bda8d9ba9cb767fb
Alexandre <alexteixeira@parceiro.sonae.pt>
2021-05-10 14:41:37 +0100

    remove unique constraints

--- assets/sql/postgresql/bootstrap/01-users.sql
+++ assets/sql/postgresql/bootstrap/01-users.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS users (
     id              SERIAL,
-    nickname        TEXT UNIQUE NOT NULL,
+    nickname        TEXT NOT NULL,
     first_name      TEXT NOT NULL,
     last_name       TEXT NOT NULL,
     country         TEXT NOT NULL,
     password        TEXT NOT NULL,
-    email           TEXT UNIQUE NOT NULL,
+    email           TEXT NOT NULL,
     disabled        BOOL DEFAULT 'f',
     version         INT DEFAULT 1,
     created_at      TIMESTAMP DEFAULT NOW(),
